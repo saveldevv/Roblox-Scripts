f29d4249289f1ea6828c99bb0ca68dc437ebaa15
Nitron <100238207+saveldevv@users.noreply.github.com>
2022-12-23 00:01:34 +0000

    Create ESP+AIMBOT.lua

---
+++ ESP+AIMBOT.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/saveldevv/Roblox-Scripts/main/UnamedESP.Lua'))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/saveldevv/Roblox-Scripts/main/Universal-AimBot.lua'))()
